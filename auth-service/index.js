b654ace0c592d844944851198a5c2a2f9c1c363d
lewis <dudou6@gmail.com>
2024-04-07 17:23:30 -0500

    add api gateway

--- auth-service/index.js
+++ auth-service/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
+const helmet = require( "helmet" );
+const morgan = require( 'morgan' ) ;
+const { createProxyMiddleware } = require( 'http-proxy-middleware' ) ;
 const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const authRouter = require('./routes/auth');
@@ -8,10 +12,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/auth', authRouter);
 
+// Middleware setup
+app.use(cors()); // Enable CORS
+app.use(helmet()); // Add security headers
+app.use(morgan("combined")); // Log HTTP requests
+app.disable("x-powered-by"); // Hide Express server information
+
 
 
 mongoose.connect(
-    'mongodb://0.0.0.0:27017/auth-service',
+    'mongodb://mongo:27017/auth-service',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
