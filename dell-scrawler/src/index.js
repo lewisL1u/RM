c778d88d2ee18002bcfca801616a00bb84d451f0
lewis <dudou6@gmail.com>
2024-05-03 17:05:44 -0500

    update content string

--- dell-scrawler/src/index.js
+++ dell-scrawler/src/index.js
@@ -3,14 +3,14 @@ const url =
   "https://outlet.us.dell.com/ARBOnlineSales/Online/InventorySearch.aspx?brandId=2801&c=us&cs=28&l=en&s=dfb&ref=cpcl_lp-us-outlet-tab-laptop-all0_cta_link_shopalllaptops";
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({devtools: true});
   const page = await browser.newPage();
-  await page.goto(url, { waitUntil: "networkidle2" });
+  await page.goto(url, { waitUntil: "networkidle2", timeout: 1000000000 });
   // Now you can use page.evaluate to scrape the data you need from the page
-  const data = await page.evaluate(() => {
-    const mainDiv = document.querySelector("#configurations");
-    const items = mainDiv ? Array.from(mainDiv.childNodes) : [];
-    return items;
+  const data = await page.$$eval("#configurations > div > ul", (uls) => {
+    return uls.map(ul => {
+      return ul.textContent?.split(/[\\n]*\s\s+/g).filter(product => product !== "");
+    });
   });
   console.log(data);
   await browser.close();
